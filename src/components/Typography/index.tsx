befa44bd48d8024203f73c82473edd0889a86048
david32145 <nascimento32145@gmail.com>
2020-06-19 10:20:33 -0300

    Add scss

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+import "./styles.scss";
+
 const Typografy: React.FC = () => {
-  return <h1>Typografy</h1>;
+  return <h1 className="md-headline">Typografy</h1>;
 };
 
 export default Typografy;
